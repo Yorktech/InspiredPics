471b1d2a0df7841264204d8c425e97555aec35f2
Yorktech Solutions <steve@minster-it.com>
2019-08-20 09:43:25 +0100

    Initial commit

---
+++ js/gallerycode.js
@@ -0,0 +1,69 @@
+﻿window.naturalGalleries = window.naturalGalleries || [];
+
+var gallery = {
+    id: 'gallery', // sends id of dom element or
+    element: document.getElementById('gallery'), // sends dom element by reference
+    options: { // Options
+
+        format: 'natural',
+
+        // Max row height. Works for both natural and square format. Prefer this option for a "responsive" approach
+        rowHeight: 350,
+
+        // Only for square format. Disables responsive
+        imagesPerRow: 2,
+
+        round: 3, // Rounded corner
+        margin: 3, // Gap between thumbnails
+        limit: 0, // Number of rows, activate pagination (disables infinite scroll)
+        minRowsAtStart: 2, // Initial number of rows. If null, gallery tries to define the number of required rows to fill the viewport.
+
+        showLabels: 'hover', // When to show the labels in thumbnails
+
+        lightbox: true, // Open a lightbox with a bigger image -> activate a zoom effect on hover on thumbnails
+        zoomRotation: true,
+
+        // Number of pixels to offset the infinite scroll autoload
+        // If negative, next rows will load before the bottom of gallery container is visible
+        // If 0 the next rows will load when the bottom of the gallery will be visible
+        // If positive, the next rows will load when the bottom of the gallery will be this amount above the end of the viewport.
+        // If positive be sure to always have this number of pixels as margin, padding or more content after the gallery.
+        infiniteScrollOffset: 0,
+
+        // Header / Search options.
+        showCount: false,
+        searchFilter: false,
+        categoriesFilter: false,
+        showNone: false,
+        showOthers: false,
+        labelCategories: 'Category',
+        labelNone: 'None',
+        labelOthers: 'Others',
+        labelSearch: 'Search',
+        labelImages: 'Images',
+    },
+    images: [
+        {
+            "thumbnail": "https://images.unsplash.com/photo-1461529959205-ba7d61debd0b?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=400&fit=max&s=0653332e9c1498112a303c583c102f6a",
+            "enlarged": "https://images.unsplash.com/photo-1461529959205-ba7d61debd0b?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=a74e25633c9c659c0778b71ca9aa33a0",
+            "title": "Antonio Ron",
+            "categories": [
+                {
+                    "id": 4,
+                    "title": "Nature",
+                    "photo_count": 41787,
+                    "links": {
+                        "self": "https://api.unsplash.com/categories/4",
+                        "photos": "https://api.unsplash.com/categories/4/photos"
+                    }
+                }
+            ],
+            "tWidth": 443.74009508716324,
+            "tHeight": 300,
+            "eWidth": 2800,
+            "eHeight": 1893
+        },
+       
+    ]
+};
+window.naturalGalleries.push(gallery);
---
+++ js/site.js
@@ -0,0 +1,131 @@
+﻿// Write your JavaScript code.
+
+$(document).ready(function() {
+
+    $('#lightbox').on('show.bs.modal',
+        function(event) {
+            var modal = $(this);
+            var picClicked = $(event.relatedTarget);
+            var dataURL = picClicked.data('uri');
+            //$('.modal-body').load(dataURL);
+            var modalImg = document.getElementById("img");
+            modalImg.src = dataURL;
+            //modal.find('.modal-title').text('New message to ' + recipient);
+            //modal.find('.modal-body input').val(recipient);
+        });
+
+
+
+    //$('.openPopup').on('click',function(){
+    //    var dataURL = $(this).attr('data-href');
+    //    $('.modal-body').load(dataURL,function(){
+    //        $('#myModal').modal({show:true});
+    //    });
+    //}); 
+});
+
+Vue.config.devtools = true;
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Welcome to Inspired Pics',
+        imageMessage: "Latest Images",
+        photos: [],
+        col1: [],
+        col2:[],
+        pageSize: 10,
+        pagedPhotos: [],
+        tags: [],
+        photo: 'none',
+        searchTag: "",
+        currentPhoto: 'none',
+        year: "2019",
+        bottom: false,
+        page: 0
+    },
+    methods: {
+        GetTagsData: function() {
+            console.log("Get tags clicked!");
+            axios.get('https://photofunctions.azurewebsites.net/api/TagList').then(response => {
+                this.photos = response.data;
+                var count = this.photos.length;
+               
+                
+            });
+
+        },
+        GetTagByName: function(name) {
+            console.log("Get tags by name:" + name);
+            axios.get('https://photofunctions.azurewebsites.net/api/GetTagByName?name=' + name).then(response => {
+                this.photos = response.data;
+                var count = this.photos.length;
+
+                this.imageMessage = "You searched for " + name;
+            });
+
+        },
+        GetPhotos: function() {
+            console.log("Top 1000 clicked!");
+            axios.get('https://photofunctions.azurewebsites.net/api/Top1000').then(response => {
+                this.photos = response.data;
+                var count = this.photos.length;
+                
+                //this.SplitData();
+            });
+        },
+        ViewPhoto: function(value) {
+            console.log("Get photo :" + value);
+            axios.get('https://photofunctions.azurewebsites.net/api/PhotoById?id=' + this.selected.id).then(
+                response => {
+                    this.photo = response.data;
+                    this.SplitData();
+                });
+        },
+        SplitData: function () { 
+            this.pagedPhotos = map(this.photos,
+                function(item, index) {
+
+                    return index % pageSize === 0 ? photos.slice(index, index + pageSize) : null;
+                })
+            .filter(function(item) { return item; })},
+        
+        bottomVisible() {
+            const scrollY = window.scrollY;
+            const visible = document.documentElement.clientHeight;
+            const pageHeight = document.documentElement.scrollHeight;
+            const bottomOfPage = visible + scrollY >= pageHeight;
+            return bottomOfPage || pageHeight < visible;
+        }
+
+
+    },
+    filters: {
+        formatDate: function (value) {
+            return moment(String(value)).format('Do MMMM YYYY');
+
+        }
+
+    },
+    mounted: function(){
+        /*window.addEventListener('scroll',
+            () => {
+                this.bottom = this.bottomVisible();
+            });*/
+        this.GetPhotos();
+
+    },
+    computed : 
+        {
+            ImagesPerCol() {
+                return this.photos /2;
+            } 
+        },
+    watch: {
+        bottom(bottom) {
+            if (bottom) {
+                this.page = this.page + 1;
+            }
+        }
+    }
+});
