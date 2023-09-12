10532cf3a812c4a6c1f8ab1dbfc0f8edebf16f30
SteveS <steve@yorktechsolutions.co.uk>
2023-09-12 16:29:10 +0000

    branch commit

---
+++ pics-svelte/src/routes/+layout.svelte
@@ -0,0 +1,54 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer class="container-fluid footer">
+		<p class="float-right"><a href="#">Back to top</a></p>
+		<p class="text-light">&copy; All text and images (c) Stephen Sawyer. </p>
+    </footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ pics-svelte/src/routes/Header.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { page } from '$app/stores';
+	
+</script>
+<header>
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+
+		<a class="navbar-brand" href="https://www.inspiredpics.co.uk">
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<img src="../images/1.png" width="64" height="64" />
+			InspiredPics
+		</a>
+
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item ">
+					<a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="images/1.png" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Main
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="terms.html">FAQ and Terms</a>
+						<a class="dropdown-item" href="about.html">About Me</a>
+						<a class="dropdown-item" href="support.html">Buy me a coffee</a>
+						<a class="dropdown-item" href="promotions.html">Promotions and advertising</a>
+
+					</div>
+				</li>
+
+
+
+
+			</ul>
+<!--                <a class="" href="#"><i class="fab fa-twitter"></i>Tweet us </a>-->
+			<a class="p-3" href="https://www.facebook.com/inspiredpicsuk/"><i class="fab fa-facebook"></i>Like us </a>
+
+		</div>
+	</nav>
+</header>
